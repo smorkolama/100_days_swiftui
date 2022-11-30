b6fbfe1e6910a966c5d4cbb8418bc28f66b1a072
Benjamin van den Hout <bvdhout@gmail.com>
2022-11-30 16:16:03 +0100

    Auto calculate

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -63,6 +63,11 @@ struct ContentView: View {
                         }
                     }
                 }
+                
+                Section {
+                    Text("Ideal bedtime: \(calculatedBedTime)")
+                        .font(.title)
+                }
             }
             .navigationTitle("BetterRest")
             .toolbar {
@@ -73,6 +78,24 @@ struct ContentView: View {
             } message: {
                 Text(alertMessage)
             }
+        }
+    }
+    
+    var calculatedBedTime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            return "Error"
         }
     }
     
