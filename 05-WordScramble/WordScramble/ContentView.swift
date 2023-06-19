85a4ae24092ddafda8adebd265b2c888e6790564
Van Den Hout Benjamin, SUB-SDC-NL-DOS-OCD <Benjamin.VanDenHout@swisscom.com>
2023-06-19 13:26:14 +0200

    Initial setup, add start.txt and ability to enter words

---
+++ 05-WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Benjamin van den Hout on 19/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+
+    var body: some View {
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                }
+
+                Section {
+                    // Note: Using id: \.self would cause problems if there were lots of duplicates in usedWords, but soon enough we’ll be disallowing that so it’s not a problem.
+                    ForEach(usedWords, id: \.self) { word in
+                        Text(word)
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+        }
+    }
+
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // Tutorial uses answer.count but this works as well right?
+        guard answer.isEmpty == false else { return }
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
