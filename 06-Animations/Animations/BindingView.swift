3d2911c893ca2a1c3c11cd6f6ca11a636b6957a9
Benjamin van den Hout <smork1@gmail.com>
2023-06-20 10:57:28 +0200

    Binding animations directly to values

--- 06-Animations/Animations/BindingView.swift
+++ 06-Animations/Animations/BindingView.swift
@@ -8,8 +8,29 @@
 import SwiftUI
 
 struct BindingView: View {
+    @State private var animationAmount = 1.0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        print(animationAmount)
+        return VStack {
+//            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+
+            Stepper("Scale amount", value: $animationAmount.animation(
+                .easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
     }
 }
 
