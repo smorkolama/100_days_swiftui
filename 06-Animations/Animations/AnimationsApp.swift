6d85b17e1d6a3bc8ad12ec9626b6260bd389e2e2
Benjamin van den Hout <smork1@gmail.com>
2023-06-20 10:51:16 +0200

    Prepare for second part of the app

--- 06-Animations/Animations/AnimationsApp.swift
+++ 06-Animations/Animations/AnimationsApp.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 @main
 struct AnimationsApp: App {
+    enum Tab {
+        case button
+        case binding
+    }
+
+    @State private var selection: Tab = .binding
+
     var body: some Scene {
+
         WindowGroup {
-            ContentView()
+            TabView(selection: $selection) {
+                ContentView()
+                    .tabItem {
+                        Label("Button", systemImage: "button.programmable")
+                    }
+                    .tag(Tab.button)
+                BindingView()
+                    .tabItem {
+                        Label("Binding", systemImage: "pencil")
+                    }
+                    .tag(Tab.binding)
+            }
         }
     }
 }
---
+++ 06-Animations/Animations/BindingView.swift
@@ -0,0 +1,20 @@
+//
+//  BindingView.swift
+//  Animations
+//
+//  Created by Benjamin van den Hout on 20/06/2023.
+//
+
+import SwiftUI
+
+struct BindingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BindingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BindingView()
+    }
+}
