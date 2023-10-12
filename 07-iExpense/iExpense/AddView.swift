c929609f2f7a58e0c910ecbeb479ad9652745430
Benjamin van den Hout <bvdhout@gmail.com>
2023-10-12 15:19:55 +0200

    AddView

---
+++ 07-iExpense/iExpense/AddView.swift
@@ -0,0 +1,47 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Benjamin van den Hout on 12/10/2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    @ObservedObject var expenses: Expenses
+
+    let types = ["Business", "Personal"]
+
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                }
+            }
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
